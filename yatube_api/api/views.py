a141ce7ebdafb1256644bd573d54492dfaaa8585
Tsekov David <tsekov.david@yandex.ru>
2022-05-22 01:17:41 +0300

    fixed viewsets

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets
+from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
@@ -44,7 +44,8 @@ class CommentViewSet(viewsets.ModelViewSet):
         return self.get_post().comments
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
 
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated, )
